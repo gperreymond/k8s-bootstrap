8f5f3059224aa562ca5d66ed218850aa86d8f839
Gilles Perreymond <gilles.perreymond@metronlab.com>
2024-05-19 09:09:35 +0200

    feat(teraform): prepare controller cluster

--- terraform/k8s-controller/00-locals.tf
+++ terraform/k8s-controller/00-locals.tf
@@ -3,4 +3,11 @@ locals {
   HELM_ARGO_EVENTS_VERSION    = "2.4.4"
   HELM_ARGO_WORKFLOWS_VERSION = "0.41.4"
   HELM_ARGO_ROLLOUTS_VERSION  = "2.35.2"
+  cluster_sources             = { for filename in fileset("../../.kubeconfigs", "*.yaml") : filename => yamldecode(file("../../.kubeconfigs/${filename}")) }
+  clusters = { for key, value in local.cluster_sources : value.clusters[0].name => {
+    clusterName = value.clusters[0].name
+    userName    = value.users[0].name
+    user        = value.users[0].user
+    cluster     = value.clusters[0].cluster
+  } }
 }
--- terraform/k8s-controller/02-argo.tf
+++ terraform/k8s-controller/02-argo.tf
@@ -14,34 +14,43 @@ resource "helm_release" "argo_cd" {
   ]
 }
 
-resource "kubernetes_manifest" "argo_cd_project_edge" {
-  manifest = yamldecode(<<YAML
-apiVersion: argoproj.io/v1alpha1
-kind: AppProject
-metadata:
-  name: edge
-  namespace: ${kubernetes_namespace.argo_system.id}
-  finalizers:
-    - resources-finalizer.argocd.argoproj.io
-spec:
-  description: All applications to deploy in each "edge" clusters
-  sourceRepos:
-    - '*'
-  namespaceResourceWhitelist:
-    - group: '*'
-      kind: '*'
-  clusterResourceWhitelist:
-    - group: '*'
-      kind: '*'
-  destinations:
-    - namespace: argo-system
-      server: https://kubernetes.default.svc
-      name: in-cluster
-    - namespace: kube-system
-      server: https://kubernetes.default.svc
-      name: in-cluster
+resource "kubernetes_secret" "argo_cd_clusters" {
+  for_each = { for cluster in local.clusters : cluster.clusterName => cluster }
+  metadata {
+    name      = each.value.clusterName
+    namespace = kubernetes_namespace.argo_system.id
+    labels = {
+      "argocd.argoproj.io/secret-type" = "cluster"
+    }
+  }
+  type = "Opaque"
+
+  data = yamldecode(<<YAML
+name: "${each.value.clusterName}"
+server: "${each.value.cluster.server}"
+config: |
+  {
+    "tlsClientConfig": {
+      "insecure": false,
+      "caData": "${each.value.cluster["certificate-authority-data"]}",
+      "certData": "${each.value.user["client-certificate-data"]}",
+      "keyData": "${each.value.user["client-key-data"]}"
+    }
+  }
 YAML
   )
+
+  depends_on = [
+    helm_release.argo_cd
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_projects" {
+  for_each = { for filepath in fileset("./argo-cd/projects", "*.yaml") : filepath => filepath }
+
+  manifest = yamldecode(templatefile("./argo-cd/projects/${each.key}", {
+    argo_cd_namespace = kubernetes_namespace.argo_system.id
+  }))
 
   depends_on = [
     helm_release.argo_cd
@@ -51,6 +60,7 @@ YAML
 resource "null_resource" "argo" {
   depends_on = [
     helm_release.argo_cd,
-    kubernetes_manifest.argo_cd_project_edge
+    kubernetes_secret.argo_cd_clusters,
+    kubernetes_manifest.argocd_projects
   ]
 }
