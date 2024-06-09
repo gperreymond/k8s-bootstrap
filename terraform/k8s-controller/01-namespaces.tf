4240289421bfd340f044da17d8b99c006861384c
Gilles Perreymond <gilles.perreymond@metronlab.com>
2024-06-09 10:27:57 +0200

    feat(argocd): prepare kuma with namespaces

--- terraform/k8s-controller/01-namespaces.tf
+++ terraform/k8s-controller/01-namespaces.tf
@@ -10,9 +10,16 @@ resource "kubernetes_namespace" "monitoring_system" {
   }
 }
 
+resource "kubernetes_namespace" "kuma_system" {
+  metadata {
+    name = "kuma-system"
+  }
+}
+
 resource "null_resource" "namespaces" {
   depends_on = [
     kubernetes_namespace.argo_system,
     kubernetes_namespace.monitoring_system,
+    kubernetes_namespace.kuma_system,
   ]
 }
